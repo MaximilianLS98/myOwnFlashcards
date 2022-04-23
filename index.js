59a8a49a606c5e8211e0f1a9d39414d70b075f6b
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2022-04-23 17:17:17 +0200

    added cards.pug, a template and learned aboutstuf

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.send('<h1>i Love Treehouse</h1>');
+    res.render('index');
 });
 
-app.get('/hello', (req, res) => {
-    res.send('<h1>Hello</h1>');
+app.get('/cards', (req, res) => {
+    res.locals.prompt = "Maximilian";
+    res.render('card', { prompt: `Who is buried in Grant's tomb?`, hint: `Think about whose tomb it is`});
 });
 
 app.listen(3000, function() {
-    console.log('Server is now running on port: 3000 at localhost')
+    console.log('Server is now running on port: 3000 at localhost');
 });

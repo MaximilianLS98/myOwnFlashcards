ef83a5a27e94d53544ab5d0d73f62d59d5067c75
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2022-04-23 21:59:35 +0200

    Installed cookieparser and made a cookie on the client on form submission

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodypaParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
+app.use(bodypaParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
@@ -10,6 +15,14 @@ app.get('/', (req, res) => {
 
 app.get('/cards', (req, res) => {
     res.render('card', { prompt: `Who is buried in Grant's tomb?`, hint: `Think about whos name is on the tomb!`});
+});
+
+app.get('/hello', (req, res) => {
+    res.render('hello', { userName: req.cookies.userName});
+});
+app.post('/hello', (req, res) => {
+    res.cookie('userName', req.body.userName);
+    res.render('hello', {userName:req.body.userName});
 });
 
 app.listen(3000, function() {

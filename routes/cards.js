71220e6fae19bd1f8a91eccecd7ed625d4729349
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2022-04-24 02:26:07 +0200

    fixing some redirects, making it so you can chose random cards. Also get specific card from url

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const { data } = require('../data/flashcardData.json');
+const { cards } = data;
 
+router.get('/', (req, res ) => {
+    const numberOfCards = cards.length;
+    const flashcardId = Math.floor( Math.random() * numberOfCards )
+    res.redirect(`/cards/${flashcardId}?side=question`)
+});
 
-router.get('/', (req, res) => {
-    res.render('card', { prompt: `Who is buried in Grant's tomb?`, hint: `Think about whos name is on the tomb!`});
+router.get('/:id', (req, res) => {
+    const { side } = req.query;
+    const { id } = req.params;
+    const text = cards[id][side];
+    const { hint } = cards[id];
+    const userName = req.cookies.userName;
+
+    const templateData = { id, text, userName };
+
+    if (side === 'question') {
+        templateData.hint = hint;
+        templateData.sideToShow = 'answer';
+        templateData.sideToShowDisplay = 'Answer';
+    } else if ( side === 'answer' ) {
+        templateData.sideToShow = 'question';
+        templateData.sideToShowDisplay = 'Question';
+    } else {
+        return res.redirect(`/cards/${id}?side=question`);
+    }
+
+    res.render('card', templateData);
 });
 
 module.exports = router;
